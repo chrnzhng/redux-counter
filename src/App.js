f77e894a8e16d8548b64baaa21e372a01d66e91c
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-06 12:22:54 -0600

    Completed attempt1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { increment, decrement } from './ducks/counter';
 import "./App.css";
 
 class App extends Component {
 	render() {
-		console.log( this.props );
+		const {
+				currentValue
+				, decrement
+				, increment
+		} = this.props;
+			
 		return (
-			<div>Hello World!</div>
-		)
+			<div className="app">
+				<section className="counter">
+					<h1 className="counter__current-value">{ currentValue }</h1>
+					<div className="counter__button-wrapper">
+						<button
+							className="counter__button"
+							onClick={ () => increment( 1 ) }
+						>
+							+1
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => increment( 5 ) }
+						>
+							+5
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => decrement( 1 ) }
+						>
+							-1
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => decrement( 5 ) }
+						>
+							-5
+						</button>
+						<br />
+						<button
+							className="counter__button"
+							disabled={ true }
+							onClick={ () => null }
+						>
+							Undo
+						</button>
+						<button
+							className="counter__button"
+							disabled={ true }
+							onClick={ () => null }
+						>
+							Redo
+						</button>
+					</div>
+				</section>
+				<section className="state">
+					<pre>
+						{ JSON.stringify( this.props, null, 2 ) }
+					</pre>
+				</section>
+			</div>
+		);
 	}
 }
+
 
 function mapStateToProps( state ) {
 	return state;
 }
 
-export default connect( mapStateToProps )( App );
+export default connect( mapStateToProps, { decrement, increment } )( App );
